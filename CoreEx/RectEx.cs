6a4d8afb2fd56b0ec59db6bebfc22623a5cd329f
Michon van Dooren <michon1992@gmail.com>
2014-06-16 18:12:15 +0200

    Fixed a few small problems with the CoreEx module.

--- CoreEx/RectEx.cs
+++ CoreEx/RectEx.cs
@@ -1,6 +1,21 @@
+using System;
+using UnityEngine;
 
 namespace MaienM.UnityUtils.CoreEx
 {
+    public enum Location
+    {
+        TOPLEFT,
+        TOP,
+        TOPRIGHT,
+        LEFT,
+        CENTER,
+        RIGHT,
+        BOTTOMLEFT,
+        BOTTOM,
+        BOTTOMRIGHT
+    }
+
     /// <summary>
     /// Extension functions for rects.
     /// </summary>
@@ -89,9 +104,10 @@ namespace MaienM.UnityUtils.CoreEx
         /// <returns>The centered rect</returns>
         public static Rect Center(this Rect baseRect, Rect relativeRect)
         {
-            return Position(baseRect, relativeRect, GUIManager.Position.CENTER, 0.5f, 0.5f);
+            return Position(baseRect, relativeRect, Location.CENTER, 0.5f, 0.5f);
         }
 
+        
         /// <summary>
         /// Position a rect within another rect.
         /// </summary>
@@ -101,50 +117,50 @@ namespace MaienM.UnityUtils.CoreEx
         /// <param name="xPct">The position on the x axis of the relativeRect to position the rect on</param>
         /// <param name="yPct">The position on the y axis of the relativeRect to position the rect on</param>
         /// <returns>The positioned rect</returns>
-        public static Rect Position(this Rect baseRect, Rect relativeRect, Position position, int xPos, int yPos)
+        public static Rect Position(this Rect baseRect, Rect relativeRect, Location location, int xPos, int yPos)
         {
             Rect newRect = new Rect(baseRect);
 
             // Position on the x axis.
-            switch (position)
+            switch (location)
             {
-                case GUIManager.Position.TOPLEFT:
-                case GUIManager.Position.LEFT:
-                case GUIManager.Position.BOTTOMLEFT:
+                case Location.TOPLEFT:
+                case Location.LEFT:
+                case Location.BOTTOMLEFT:
                     newRect.x = relativeRect.x + xPos;
                     break;
 
-                case GUIManager.Position.TOP:
-                case GUIManager.Position.CENTER:
-                case GUIManager.Position.BOTTOM:
+                case Location.TOP:
+                case Location.CENTER:
+                case Location.BOTTOM:
                     newRect.x = relativeRect.x + xPos - baseRect.width / 2;
                     break;
 
-                case GUIManager.Position.TOPRIGHT:
-                case GUIManager.Position.RIGHT:
-                case GUIManager.Position.BOTTOMRIGHT:
+                case Location.TOPRIGHT:
+                case Location.RIGHT:
+                case Location.BOTTOMRIGHT:
                     newRect.x = relativeRect.x + xPos - baseRect.width;
                     break;
             }
 
             // Position on the y axis.
-            switch (position)
+            switch (location)
             {
-                case GUIManager.Position.TOPLEFT:
-                case GUIManager.Position.TOP:
-                case GUIManager.Position.TOPRIGHT:
+                case Location.TOPLEFT:
+                case Location.TOP:
+                case Location.TOPRIGHT:
                     newRect.y = relativeRect.y + yPos;
                     break;
 
-                case GUIManager.Position.LEFT:
-                case GUIManager.Position.CENTER:
-                case GUIManager.Position.RIGHT:
+                case Location.LEFT:
+                case Location.CENTER:
+                case Location.RIGHT:
                     newRect.y = relativeRect.y + yPos - baseRect.height / 2;
                     break;
 
-                case GUIManager.Position.BOTTOMLEFT:
-                case GUIManager.Position.BOTTOM:
-                case GUIManager.Position.BOTTOMRIGHT:
+                case Location.BOTTOMLEFT:
+                case Location.BOTTOM:
+                case Location.BOTTOMRIGHT:
                     newRect.y = relativeRect.y + yPos - baseRect.height;
                     break;
             }
@@ -157,13 +173,13 @@ namespace MaienM.UnityUtils.CoreEx
         /// </summary>
         /// <param name="baseRect">The rect to position</param>
         /// <param name="relativeRect">The rect to be positioned in</param>
-        /// <param name="layout">The relative position of the baseRect to position on</param>
+        /// <param name="location">The relative position of the baseRect to position on</param>
         /// <param name="xPct">The percentage of the x axis of the relativeRect to position the rect on</param>
         /// <param name="yPct">The percentage of the y axis of the relativeRect to position the rect on</param>
         /// <returns>The positioned rect</returns>
-        public static Rect Position(this Rect baseRect, Rect relativeRect, Position layout, float xPct, float yPct)
+        public static Rect Position(this Rect baseRect, Rect relativeRect, Location location, float xPct, float yPct)
         {
-            return Position(baseRect, relativeRect, layout, (int)Math.Round(relativeRect.width * xPct, 0), (int)Math.Round(relativeRect.height * yPct, 0));
+            return Position(baseRect, relativeRect, location, (int)Math.Round(relativeRect.width * xPct, 0), (int)Math.Round(relativeRect.height * yPct, 0));
         }
 
         /// <summary>
--- CoreEx/Vector2Ex.cs
+++ CoreEx/Vector2Ex.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 namespace MaienM.UnityUtils.CoreEx
 {
     /// <summary>
